340ed243d2cf6991b67b364baad7d87a6431241e
Cédric Darné <cedric.darne@gmail.com>
2014-01-16 00:19:23 +0100

    Instantiate and move shots

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mover : MonoBehaviour
+{
+    public float speed;
+
+    void Start()
+    {
+        rigidbody.velocity = Vector3.forward * speed;
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -12,6 +12,21 @@ public class PlayerController : MonoBehaviour
     public float speed;
     public float tilt;
     public Boundary boundary;
+
+    public GameObject shot;
+    public Transform shotSpawn;
+    public float fireRate;
+
+    private float nextShot = 0f;
+       
+    void Update() {
+        if(Input.GetButton("Fire1") && Time.time > nextShot)
+        {
+            nextShot = Time.time + fireRate;
+            Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
+        }
+
+    }
     
     void FixedUpdate()
     {
