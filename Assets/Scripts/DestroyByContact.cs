c9b763481b76ed02d675de127076b9ea42d047cd
Cédric Darné <cedric.darne@gmail.com>
2014-01-16 22:53:51 +0100

    Asteroid
    - Random rotation
    - Destroy by contact

---
+++ Assets/Scripts/DestroyByContact.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyByContact : MonoBehaviour
+{
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Boundary"))
+        {
+            return;
+        }
+        Destroy(other.gameObject);
+        Destroy(gameObject);
+    }
+}
