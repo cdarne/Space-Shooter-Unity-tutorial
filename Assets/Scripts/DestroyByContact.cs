0c271814683c7d3bb2a1c5103633c1b4d876198a
Cédric Darné <cedric.darne@c4mprod.com>
2014-01-17 14:10:41 +0100

    Explosions & GameController

--- Assets/Scripts/DestroyByContact.cs
+++ Assets/Scripts/DestroyByContact.cs
@@ -3,12 +3,20 @@ using System.Collections;
 
 public class DestroyByContact : MonoBehaviour
 {
+    public GameObject explosion;
+    public GameObject playerExplosion;
 
     void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Boundary"))
         {
             return;
+        }
+
+        Instantiate(explosion, transform.position, transform.rotation);
+        if (other.collider.CompareTag("Player"))
+        {
+            Instantiate(playerExplosion, other.transform.position, other.transform.rotation);
         }
         Destroy(other.gameObject);
         Destroy(gameObject);
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour
+{
+
+    public GameObject hazard;
+    public Vector3 spawnValues;
+
+    void Start()
+    {
+        SpawnWaves();
+    }
+
+    void SpawnWaves()
+    {
+        Instantiate(hazard, new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z), Quaternion.identity);
+    }
+}
