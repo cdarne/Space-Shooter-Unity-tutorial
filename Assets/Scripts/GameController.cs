0609f1fc37fd94f2b2b20870c370efacfa45bb44
Cédric Darné <cedric.darne@c4mprod.com>
2014-01-20 13:37:03 +0100

    Spawing Waves of asteroids, explosions clean up

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -6,14 +6,27 @@ public class GameController : MonoBehaviour
 
     public GameObject hazard;
     public Vector3 spawnValues;
+    public int hazardCount;
+    public float spawnWait;
+    public float startWait;
+    public float waveWait;
 
     void Start()
     {
-        SpawnWaves();
+        StartCoroutine(SpawnWaves());
     }
 
-    void SpawnWaves()
+    IEnumerator SpawnWaves()
     {
-        Instantiate(hazard, new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z), Quaternion.identity);
+        yield return new WaitForSeconds(startWait);
+        while (true)
+        {
+            for (int i = 0; i < hazardCount; i++)
+            {
+                Instantiate(hazard, new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z), Quaternion.identity);
+                yield return new WaitForSeconds(spawnWait);
+            }
+            yield return new WaitForSeconds(waveWait);
+        }
     }
 }
